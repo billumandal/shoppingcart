ef81e01e14f13247ce68cad996d2347cff3a9da8
billumandal <billu.mandal@gmail.com>
2016-08-20 18:10:17 +0530

    Nothing to add

--- shoppingcart/urls.py
+++ shoppingcart/urls.py
@@ -22,32 +22,14 @@ from django.contrib.auth import urls, views
 from django.views.generic import TemplateView
 
 urlpatterns = [ 
-    url(r'^$', views.home, name='home'),
+    # url(r'^$', cart.views.home, name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^register_activate/', include('register_activate.urls')),
     url(r'^productlist/$', ListProductView.as_view(), name='productlist'),
-    # url(r'^transactionlist/$', ListTransactionView.as_view(), name='transactionlist'),
+    url(r'^transactionlist/$', ListTransactionView.as_view(), name='transactionlist'),
     url(r'^$', TemplateView.as_view(template_name='index.html' ), name='home'),
     url(r'^usertest/$', TemplateView.as_view(template_name='./registration/usertest.html' ), name='usertest'),
     
-    # ____________________________________________________________________________________________________________
-    # url(r'^login/$', 'django.contrib.auth.views.login', name='auth_login'),
-    # url(r'^logout/$', 'django.contrib.auth.views.logout', name='auth_logout'),
-
-    # url(r'^password_change/$', 'django.contrib.auth.views.PasswordChangeView.as_view()', name='password_change'),
-    # url(r'^password_change/done/$', 'django.contrib.auth.views.PasswordChangeDoneView.as_view()', name='password_change_done'),
-    # url(r'^password_reset/$', 'PasswordResetView.as_view()', name='password_reset'),
-    # url(r'^password_reset/done/$', 'django.contrib.auth.views.PasswordResetDoneView.as_view()', name='password_reset_done'),
-    # url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-    #     'django.contrib.auth.views.PasswordResetConfirmView.as_view()', name='password_reset_confirm'),
-    # url(r'^reset/done/$', 'django.contrib.auth.views.PasswordResetCompleteView.as_view()', name='password_reset_complete'),
-    # url(r'^$', TemplateView.as_view(template_name='password_change_form.html' ), name='auth_password_change'),
-    # url(r'^change_password/$', 'django.contrib.auth.views.logout', name='auth_password_change'),
-    # ____________________________________________________________________________________________________________
-
-    # url(r'^register/$', 'register'),
-    # url(r'^activate/(?P<key>.+)$', 'activation'),
-    # url(r'^new-activation-link/(?P<user_id>\d+)/$', 'new_activation_link'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
