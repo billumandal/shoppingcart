507edc65f8dac380be2d01cd7beb211816b9f474
billumandal <billu.mandal@gmail.com>
2016-09-07 15:02:40 +0530

    Seems i need to commit thes tash, dunno what'll happen

--- dragimage/models.py
+++ dragimage/models.py
@@ -4,4 +4,8 @@ from shoppingcart import settings
 import os
 
 class UploadFile(models.Model):
+<<<<<<< Updated upstream
     file = models.FileField(upload_to='BASE_DIR/product_pictures/%Y/%m/%d')
+=======
+    file = models.FileField(upload_to='~/../product_pictures/%Y/%m/%d')
+>>>>>>> Stashed changes
